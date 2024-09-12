949f4e5a63b1424d02f5af3e361ea9120a5dda67
Nicolas Burbano <rnbp@Nicolass-MacBook-Pro.local>
2024-09-12 14:21:22 -0500

    ensure email format

--- src/models/User.js
+++ src/models/User.js
@@ -3,14 +3,14 @@ import bcrypt from 'bcryptjs';
 
 
 const UserSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true, lowercase: true, trim: true },
     password: { type: String },
     googleId: { type: String },
     name: { type: String },
     app_score: { type: Number, default: 0 },
     createdAt: { type: Date, default: Date.now },
     device: { type: String },
-    verified: { type: Boolean, default:false },
+    verified: { type: Boolean, default: false },
     resetToken: String,
     resetTokenExpiration: Date,
 });
