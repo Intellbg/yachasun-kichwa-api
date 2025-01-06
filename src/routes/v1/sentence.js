d2e3694cfcadb21812268ae83e9285ffddc70685
Fausto Universidad Roman <fausto.roman@epn.edu.ec>
2025-01-05 22:01:11 -0500

    Se agrego otro enlace para el controller de sentence

--- src/routes/v1/sentence.js
+++ src/routes/v1/sentence.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createSentenceEntry, getSentenceList, getSentenceEntry, patchSentenceEntry, deleteSentenceEntry, getQuestion, getQuestionTime } from '../../controllers/v1/sentenceController.js';
+import { createSentenceEntry, getSentenceList, getSentenceEntry, patchSentenceEntry, deleteSentenceEntry, getQuestion, getQuestionTime, getSentence } from '../../controllers/v1/sentenceController.js';
 
 const router = express.Router();
 
 router.get('/get-question', getQuestion);
 router.get('/get-question-time', getQuestionTime);
+router.get('/get-sentence', getSentence);
 router.get('/', getSentenceList);
 router.post('/', createSentenceEntry);
 router.get('/:id', getSentenceEntry);
